2d3a317e878b1a9250903bdd593ac8b3043b0acd
Frexirus <admin@frexirus.ru>
2024-06-21 20:18:52 +0300

    Update obs-tally-sender.py

--- obs-tally-sender.py
+++ obs-tally-sender.py
@@ -2,13 +2,34 @@ import obspython as obs
 import requests
 from threading import Thread
 
-# IP адрес приёмника
-TARGET_IP = '192.168.25.55'
+# IP адрес приёмника по умолчанию
+TARGET_IP_PROPERTY = '192.168.25.55'
 
 # Цвета в HEX
 RED_HEX = '#FF0000'
 GREEN_HEX = '#00FF00'
 WHITE_HEX = '#FFFFFF'
+
+TARGET_SCENE_NAME_PROPERTY = 'HANDYCAM'
+
+def script_properties():
+    props = obs.obs_properties_create()
+
+    obs.obs_properties_add_text(props, TARGET_IP_PROPERTY, "IP адрес приёмника TALLY", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_text(props, TARGET_SCENE_NAME_PROPERTY, "Имя сцены для которой предназначен сигнал", obs.OBS_TEXT_DEFAULT)
+
+    return props
+
+def script_update(settings):
+    global TARGET_IP
+    global TARGET_SCENE_NAME
+
+    TARGET_IP = obs.obs_data_get_string(settings, TARGET_IP_PROPERTY)
+    TARGET_SCENE_NAME = obs.obs_data_get_string(settings, TARGET_SCENE_NAME_PROPERTY)
+
+def script_defaults(settings):
+    obs.obs_data_set_default_string(settings, TARGET_IP_PROPERTY, '192.168.25.55')
+    obs.obs_data_set_default_string(settings, TARGET_SCENE_NAME_PROPERTY, 'HANDYCAM')
 
 def handle_event(event):
     if event is obs.OBS_FRONTEND_EVENT_SCENE_CHANGED:
@@ -19,11 +40,11 @@ def handle_event(event):
         get_preview_scene = obs.obs_frontend_get_current_preview_scene()
         preview_scene = obs.obs_source_get_name(get_preview_scene)
         # Проверка на наличие слова HANDYCAM
-        if 'HANDYCAM' in current_scene:
+        if TARGET_SCENE_NAME in current_scene:
             # Присваивание переменной color красного цвета
             color = RED_HEX
             #send_color(RED_HEX)
-        elif 'HANDYCAM' in preview_scene:
+        elif TARGET_SCENE_NAME in preview_scene:
             # Присваивание переменной color зелёного цвета
             color = GREEN_HEX
             #send_color(GREEN_HEX)
@@ -46,6 +67,6 @@ def send_color(hex_color):
     try:
         requests.get(url, timeout=2)
     except:
-        obs.script_log(obs.LOG_INFO, 'Нет связи с приёмником...')
+        obs.script_log(obs.LOG_INFO, f'Нет связи с приёмником {TARGET_IP}...')
 
 obs.obs_frontend_add_event_callback(handle_event)
