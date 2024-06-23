d2c0429421a639d2ce750577af518e64168dc2cc
Frexirus <admin@frexirus.ru>
2024-06-23 18:41:06 +0300

    Update obs-tally-sender.py

--- obs-tally-sender.py
+++ obs-tally-sender.py
@@ -4,37 +4,50 @@ from threading import Thread
 import re
 
 # IP адрес приёмника по умолчанию
-TARGET_IP_PROPERTY = '192.168.25.55'
+target_ip_property = '192.168.25.55'
+# Префикс имени сцен
+target_scene_name_property = 'TL'
 
 # Цвета в HEX
 RED_HEX = 'FF0000'
 GREEN_HEX = '00FF00'
 WHITE_HEX = 'FFFFFF'
 
-TARGET_SCENE_NAME_PROPERTY = 'HANDYCAM'
+def script_description():
+	return """
+	Передача HTTP GET запроса на TALLY приёмник.
+	Для тестирования подключения используйте журнал скрипта. 
+	В случае, если у вас появляется ошибка при закрытии OBS, 
+	попробуйте удалить из имён сцен все пробелы.
+	"""
+
 
 def script_properties():
     props = obs.obs_properties_create()
 
-    obs.obs_properties_add_text(props, TARGET_IP_PROPERTY, "IP адрес приёмника TALLY", obs.OBS_TEXT_DEFAULT)
-    obs.obs_properties_add_text(props, TARGET_SCENE_NAME_PROPERTY, "Начало имени сцены для которой предназначен сигнал", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_text(props, target_ip_property, "IP адрес приёмника TALLY", obs.OBS_TEXT_DEFAULT)
     obs.obs_properties_add_button(props, "", "Проверить подключение", test_connect)
+    obs.obs_properties_add_text(props, target_scene_name_property, "Префикс имени сцены для которой предназначен сигнал", obs.OBS_TEXT_DEFAULT)
+
+    obs.obs_properties_add_color(props, "tally^IdleColor", "Idle Color")
+    obs.obs_properties_add_color(props, "tally^PreviewColor", "Queued Color")
+    obs.obs_properties_add_color(props, "tally^ProgramColor", "Live Color")
 
     return props
 
 def script_update(settings):
-    global TARGET_IP
-    global TARGET_SCENE_NAME
+    global target_ip
+    global target_scene_name
 
-    TARGET_IP = obs.obs_data_get_string(settings, TARGET_IP_PROPERTY)
-    TARGET_SCENE_NAME = obs.obs_data_get_string(settings, TARGET_SCENE_NAME_PROPERTY)
+    target_ip = obs.obs_data_get_string(settings, target_ip_property)
+    target_scene_name = obs.obs_data_get_string(settings, target_scene_name_property)
 
 def script_defaults(settings):
-    obs.obs_data_set_default_string(settings, TARGET_IP_PROPERTY, '192.168.25.55')
-    obs.obs_data_set_default_string(settings, TARGET_SCENE_NAME_PROPERTY, 'HANDYCAM')
+    obs.obs_data_set_default_string(settings, target_ip_property, '192.168.25.55')
+    obs.obs_data_set_default_string(settings, target_scene_name_property, 'TL')
 
 def handle_event(event):
-    if (event is obs.OBS_FRONTEND_EVENT_SCENE_CHANGED) or (event is obs.OBS_FRONTEND_EVENT_PREVIEW_SCENE_CHANGED):
+    if event is obs.OBS_FRONTEND_EVENT_PREVIEW_SCENE_CHANGED:
         global color
         # Получение информации о текущей сцене
         get_current_scene = obs.obs_frontend_get_current_scene()
@@ -42,14 +55,14 @@ def handle_event(event):
         # Получение информации о сцене в окне превью
         get_preview_scene = obs.obs_frontend_get_current_preview_scene()
         preview_scene = obs.obs_source_get_name(get_preview_scene)
-        # Проверка на наличие слова HANDYCAM
+        # Проверка на наличие префикса в имени сцены
         #if TARGET_SCENE_NAME in current_scene:
-        if re.search(f"^{TARGET_SCENE_NAME}*", current_scene):
+        if re.search(f"^{target_scene_name}*", current_scene):
             # Присваивание переменной color красного цвета
             color = RED_HEX
             #send_color(RED_HEX)
         #elif TARGET_SCENE_NAME in preview_scene:
-        elif re.search(f"^{TARGET_SCENE_NAME}*", preview_scene):
+        elif re.search(f"^{target_scene_name}*", preview_scene):
             # Присваивание переменной color зелёного цвета
             color = GREEN_HEX
             #send_color(GREEN_HEX)
@@ -65,20 +78,25 @@ def handle_event(event):
         obs.script_log(obs.LOG_INFO, 'CURRENT: ' + current_scene)
         obs.script_log(obs.LOG_INFO, 'PREVIEW: ' + preview_scene)
 
+    elif event is obs.OBS_FRONTEND_EVENT_SCRIPTING_SHUTDOWN:
+        # Выполнить при завершении работы скрипта (Закрытие OBS)
+        th = Thread(target=send_color, args=('000000',))
+        th.start()
+        obs.script_log(obs.LOG_INFO, 'TEST')
+
+
 def test_connect(props, prop):
-    #url = f'http://{TARGET_IP}/set?color={color}'
-    #requests.get(url, timeout=2)
     th = Thread(target=send_color, args=(color,))
     th.start()
 def send_color(hex_color):
     """Посылка GET запроса с заданным цветом."""
-    url = f'http://{TARGET_IP}/set?color={hex_color}'
+    url = f'http://{target_ip}/set?color={hex_color}'
     obs.script_log(obs.LOG_INFO, url)
     try:
         requests.get(url, timeout=2)
     except:
-        obs.script_log(obs.LOG_INFO, f'Нет связи с приёмником {TARGET_IP}...')
+        obs.script_log(obs.LOG_INFO, f'Нет связи с приёмником {target_ip}...')
     else:
-        obs.script_log(obs.LOG_INFO, f'Связь с приёмником {TARGET_IP} установлена...')
+        obs.script_log(obs.LOG_INFO, f'Связь с приёмником {target_ip} установлена...')
 
 obs.obs_frontend_add_event_callback(handle_event)
